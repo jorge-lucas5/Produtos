aa3410620877a71822dfa34ff9531c4c2186b271
jorgelucas <jorgelucasl91@gmail.com>
2021-03-25 00:51:37 -0300

    realizando upload de imagens

--- src/Estudos.App.Web/Controllers/ProdutoController.cs
+++ src/Estudos.App.Web/Controllers/ProdutoController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using AutoMapper;
 using Estudos.App.Business.Interfaces;
 using Estudos.App.Business.Models;
+using Estudos.App.Web.Util;
 using Microsoft.AspNetCore.Mvc;
 using Estudos.App.Web.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
 
 namespace Estudos.App.Web.Controllers
 {
@@ -15,12 +20,14 @@ namespace Estudos.App.Web.Controllers
         private readonly IProdutoRepository _produtoRepository;
         private readonly IFornecedorRepository _fornecedorRepository;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
 
-        public ProdutoController(IProdutoRepository produtoRepository, IMapper mapper, IFornecedorRepository fornecedorRepository)
+        public ProdutoController(IProdutoRepository produtoRepository, IMapper mapper, IFornecedorRepository fornecedorRepository, IConfiguration configuration)
         {
             _produtoRepository = produtoRepository;
             _mapper = mapper;
             _fornecedorRepository = fornecedorRepository;
+            _configuration = configuration;
         }
 
         #region Actions
@@ -45,7 +52,7 @@ namespace Estudos.App.Web.Controllers
         public async Task<IActionResult> Create()
         {
             var produtoViewModel = await PopularFornecedores(new ProdutoViewModel());
-            ViewBag.FornecedorId = new SelectList(produtoViewModel.Fornecedores,"Id","Nome");
+            ViewBag.FornecedorId = new SelectList(produtoViewModel.Fornecedores, "Id", "Nome");
             return View(produtoViewModel);
         }
 
@@ -54,17 +61,22 @@ namespace Estudos.App.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProdutoViewModel produtoViewModel)
         {
-            if (!ModelState.IsValid)
+            var caminho = await FileHelper.UploadArquivo(produtoViewModel.ImagemUpload, _configuration);
+            if (!ModelState.IsValid || string.IsNullOrEmpty(caminho))
             {
                 produtoViewModel = await PopularFornecedores(produtoViewModel);
+                ViewBag.FornecedorId = new SelectList(produtoViewModel.Fornecedores, "Id", "Nome");
                 return View(produtoViewModel);
             }
-
+            
             var produto = _mapper.Map<Produto>(produtoViewModel);
+            produto.Imagem = caminho;
             await _produtoRepository.Adicionar(produto);
             return RedirectToAction(nameof(Index));
 
         }
+
+
 
         public async Task<IActionResult> Edit(Guid id)
         {
@@ -130,6 +142,8 @@ namespace Estudos.App.Web.Controllers
                 _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
             return produto;
         }
+
+
 
         #endregion
     }
---
+++ src/Estudos.App.Web/Util/FileHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Estudos.App.Web.Util
+{
+    public class FileHelper
+    {
+        public static async Task<string> UploadArquivo(IFormFile arquivo, IConfiguration configuration,  string imgPrefixo = null)
+        {
+            if (string.IsNullOrEmpty(imgPrefixo))
+                imgPrefixo = GerarPrefixo();
+
+            if (arquivo.Length <= 0) return null;
+
+            var uploadFilePath = configuration.GetValue("UploadFilePath", "wwwroot/uploadImagens");
+            imgPrefixo += arquivo.FileName;
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), uploadFilePath, imgPrefixo);
+
+            if (System.IO.File.Exists(path)) return null;
+
+            using (var stream = new FileStream(path,FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
+
+            return imgPrefixo;
+
+
+        }
+
+        public static string GerarPrefixo()
+        {
+            return Guid.NewGuid() + "_";
+        }
+    }
+}
