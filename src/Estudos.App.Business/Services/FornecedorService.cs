828e0879107d9f669f053a18da542d3d92a1a641
jorgelucas <jorgelucasl91@gmail.com>
2021-03-25 18:54:36 -0300

    validação de fornecedor

---
+++ src/Estudos.App.Business/Services/FornecedorService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Estudos.App.Business.Interfaces;
+using Estudos.App.Business.Models;
+using Estudos.App.Business.Models.Validations;
+
+namespace Estudos.App.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            //validar o estar da entidade
+            if(!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+            && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+            
+            //validar se não existe fornecedor com o msm documento
+            return;
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            if (!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
