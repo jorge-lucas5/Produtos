1d899f09d123c875fa1920c5bc6da8a39a4d985f
jorgelucas <jorgelucasl91@gmail.com>
2021-03-24 21:05:48 -0300

    criando badgeBooleanTagHelper

---
+++ src/Estudos.App.Web/Extensions/BadgeBooleanTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Estudos.App.Web.Extensions
+{
+    public class BadgeBooleanTagHelper : TagHelper
+    {
+        public bool Ativo { get; set; } = true;
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var classe = "badge badge-success";
+            var texto = "Sim";
+            if (!Ativo)
+            {
+                classe = "badge badge-danger";
+                texto = "Não";
+            }
+            output.TagName = "span";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent();
+            output.Attributes.SetAttribute("class", classe);
+
+            if (!string.IsNullOrEmpty(target))
+                texto = target;
+            output.Content.SetContent(texto);
+        }
+    }
+}
