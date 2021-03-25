b202af54a9897e9d4ed54df6a6a86b15ff4a1322
jorgelucas <jorgelucasl91@gmail.com>
2021-03-25 18:54:49 -0300

    validação de produto

---
+++ src/Estudos.App.Business/Services/ProdutoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Estudos.App.Business.Interfaces;
+using Estudos.App.Business.Models;
+using Estudos.App.Business.Models.Validations;
+
+namespace Estudos.App.Business.Services
+{
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
