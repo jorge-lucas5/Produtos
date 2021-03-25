f674ed70b7fc1258a58ae1df6382b2d38941a96a
jorgelucas <jorgelucasl91@gmail.com>
2021-03-25 18:54:03 -0300

    implementação da classe responsável por lançar as notificações de validações do FluentValidation

---
+++ src/Estudos.App.Business/Services/BaseService.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Estudos.App.Business.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Estudos.App.Business.Services
+{
+    public abstract class BaseService
+    {
+        protected void Notificar(string mensagem)
+        {
+            //propagar erro até a camada de apresentação
+        }
+
+        protected void Notificar(ValidationResult validationResult)
+        {
+            validationResult.Errors.ToList()
+                .ForEach(a => Notificar(a.ErrorMessage));
+        }
+
+        protected bool ExecutarValidacao<TValidacao, TEntidade>(TValidacao validacao, TEntidade entidade)
+            where TValidacao : AbstractValidator<TEntidade>
+            where TEntidade : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+            return false;
+        }
+    }
+}
