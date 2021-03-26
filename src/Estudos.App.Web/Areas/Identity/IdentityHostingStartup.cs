a2b9d2f7a9934ba85df5128ce75f68526ca5057f
jorgelucas <jorgelucasl91@gmail.com>
2021-03-26 16:24:29 -0300

    add Identity Pages

---
+++ src/Estudos.App.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Estudos.App.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Estudos.App.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Estudos.App.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
